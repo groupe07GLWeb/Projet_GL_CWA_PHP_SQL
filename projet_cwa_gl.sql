f33f02431c3baa3b6b4b0975893846f8f74ac09b
Votre ATCGeraldine <atchadegeraldine@gmail.com>
2021-01-09 12:58:14 +0100

    mise à jour de la base de donnée

--- projet_cwa_gl.sql
+++ projet_cwa_gl.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 4.9.5deb2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost
--- Généré le : Dim 03 jan. 2021 à 16:36
--- Version du serveur :  10.4.14-MariaDB
--- Version de PHP : 7.2.34
+-- Hôte : localhost:3306
+-- Généré le : sam. 09 jan. 2021 à 12:53
+-- Version du serveur :  8.0.22-0ubuntu0.20.04.3
+-- Version de PHP : 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -28,13 +29,13 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `anomalie` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `date_intervention` date NOT NULL,
   `descriptif_anomalie` varchar(100) NOT NULL,
   `resolu` tinyint(1) NOT NULL,
   `operation_effectue` varchar(100) NOT NULL,
   `date_anomalie` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -43,14 +44,21 @@ CREATE TABLE `anomalie` (
 --
 
 CREATE TABLE `cereale` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `type` varchar(100) NOT NULL,
-  `poids` int(11) NOT NULL,
+  `poids` int NOT NULL,
   `date_arrivage` date NOT NULL,
   `date_expedition` date NOT NULL,
-  `num_cellule` int(11) NOT NULL,
-  `qualité` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `num_cellule` int NOT NULL,
+  `qualite` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `cereale`
+--
+
+INSERT INTO `cereale` (`id`, `type`, `poids`, `date_arrivage`, `date_expedition`, `num_cellule`, `qualite`) VALUES
+(1, 'Ble', 500, '2021-01-26', '2021-01-30', 2, 'Bonne');
 
 -- --------------------------------------------------------
 
@@ -59,24 +67,23 @@ CREATE TABLE `cereale` (
 --
 
 CREATE TABLE `equipement` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `nom_equipement` varchar(100) NOT NULL,
   `operation` varchar(100) NOT NULL,
   `etat` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `equipement`
 --
 
-INSERT INTO `equipement` (`id`, `nom_equipement`, `operation`, `etat`) 
-VALUES (1, 'Nettoyeur_Separateur', 'none', '1'), 
-(2, 'Silo', 'none', '1'), 
-(3, 'Boisseaux', 'none', '0'), 
-(4, 'Grille_Roulage', 'none', '1'), 
-(5, 'Tremie', 'none', '0'), 
-(6, 'Elevateur', 'none', '1');
+INSERT INTO `equipement` (`id`, `nom_equipement`, `operation`, `etat`) VALUES
+(1, 'Nettoyeur_Separateur', 'none', 1),
+(2, 'Silo', 'none', 1),
+(3, 'Boisseaux', 'none', 0),
+(4, 'Grille_Roulage', 'none', 1),
+(5, 'Tremie', 'none', 0),
+(6, 'Elevateur', 'none', 1);
 
 -- --------------------------------------------------------
 
@@ -85,11 +92,11 @@ VALUES (1, 'Nettoyeur_Separateur', 'none', '1'),
 --
 
 CREATE TABLE `operation` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `nettoyage` varchar(100) NOT NULL,
   `ventilation` varchar(100) NOT NULL,
   `injectionPesticide` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -98,13 +105,13 @@ CREATE TABLE `operation` (
 --
 
 CREATE TABLE `utilisateur` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `nom` varchar(100) NOT NULL,
   `prenom` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `role` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `utilisateur`
@@ -120,12 +127,12 @@ INSERT INTO `utilisateur` (`id`, `nom`, `prenom`, `password`, `email`, `role`) V
 --
 
 CREATE TABLE `variable` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `temperature` double NOT NULL,
   `humidite` double NOT NULL,
   `poussiere` double NOT NULL,
   `insecticide` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `variable`
@@ -183,37 +190,37 @@ ALTER TABLE `variable`
 -- AUTO_INCREMENT pour la table `anomalie`
 --
 ALTER TABLE `anomalie`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `cereale`
 --
 ALTER TABLE `cereale`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `equipement`
 --
 ALTER TABLE `equipement`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pour la table `operation`
 --
 ALTER TABLE `operation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `variable`
 --
 ALTER TABLE `variable`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
